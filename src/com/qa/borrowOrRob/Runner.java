fe1dbd6ef535b83e3a6947094ff94eca29d56948
Johannes <Johannes.Bull@academytrainee.com>
2020-02-19 10:38:25 +0000

    Added palindrome exercise

--- src/com/qa/borrowOrRob/Runner.java
+++ src/com/qa/borrowOrRob/Runner.java
@@ -3,7 +3,8 @@ package com.qa.borrowOrRob;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BorrowOrRob example = new BorrowOrRob();
+		System.out.println(example.borrowOrRob("Racecar"));
 
 	}
 
